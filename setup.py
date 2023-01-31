faebff2e5fa2f49d9decb069425f64a950c7f045
WxGuy <wrf.guy@gmail.com>
2023-02-01 05:05:55 +0530

    Update to version 3.4.0
    Shifted to build module for pypi release
    
    Added black as part of pyproject.toml configuration
    
    Auto detection of version from gith tag so that it can be released directly from github repo

--- setup.py
+++ setup.py
@@ -68,10 +68,26 @@ def list_files(directory):
             files.append(_file)
     return files
 
+def _version():
+    from setuptools_scm.version import SEMVER_MINOR, guess_next_simple_semver, release_branch_semver_version
+
+    def my_release_branch_semver_version(version):
+        v = release_branch_semver_version(version)
+        if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_MINOR):
+            return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_MINOR)
+        return v
+
+    return {
+        'version_scheme': my_release_branch_semver_version,
+        'local_scheme': 'no-local-version',
+    }
+
+
 setup(
     name=NAME,
-    version=about['__version__'],
-    use_scm_version={'version_scheme': 'python-simplified-semver'},
+    # version=about['__version__'],
+    # use_scm_version={'version_scheme': 'no-guess-dev'},
+    use_scm_version=_version,
     setup_requires=['setuptools_scm'],
     description=DESCRIPTION,
     long_description=long_description,
